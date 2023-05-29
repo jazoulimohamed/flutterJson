fdb65cbe17b21f7a049ef11c01b32a689cce90c8
Mohamed Jazouli <midojaz113@gmail.com>
2023-05-29 15:07:12 +0100

    test

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:tp1/screen/Acueil .dart';
+import 'package:tp1/screen/M.dart ';
+import 'package:tp1/screen/Operation.dart';
+
+Future<void> afficherSecond(int s)async {
+  for(var i=0; i<=s;i++){
+    await Future.delayed(Duration(seconds: 5),()=>{
+      print(i)});
+  }
+}
+
+void main() {
+  //runApp(s() ) ;
+  afficherSecond(12);
+}
+
+class s extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Calcul",
+      home: Operation(), //Text("page 1") ,
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      routes:{} ,
+
+
+
+    );
+  }
+
+}
+
+
+
+class js extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "List Json",
+      home: JsonClass(), //Text("page 1") ,
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      routes:{} ,
+
+
+
+    );
+  }
+
+}
+class f1 extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "bienvenu",
+      home: Acueil(), //Text("page 1") ,
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      routes:{} ,
+
+
+
+    );
+  }
+
+}
